3737cb8f4281de2143a474b7592779fae17d448e
Charchit <Untamedcharchit@gmail.com>
2024-04-27 23:32:04 +0530

    Some Updates on Details Page and MainActivity for fixed font size

--- app/src/main/java/com/example/passwordmanagerapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/passwordmanagerapp/activities/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.passwordmanagerapp.activities
 
+import android.content.Context
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -15,5 +17,12 @@ class MainActivity : AppCompatActivity() {
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
 
 
+    }
+
+    override fun attachBaseContext(newBase: Context?) {
+        super.attachBaseContext(newBase)
+        val newOverride = Configuration(newBase?.resources?.configuration)
+        newOverride.fontScale = 1.0f
+        applyOverrideConfiguration(newOverride)
     }
 }
